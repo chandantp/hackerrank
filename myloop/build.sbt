32bd5843c346d3a7c964f1946ecfc4d8ef3ca79d
Chandan TP <chandan.tp@gmail.com>
2017-07-31 15:50:25 +1000

    Examples of building new control structures in Scala

---
+++ myloop/build.sbt
@@ -0,0 +1,11 @@
+name := "MyLoop"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+// logLevel := Level.Debug
+
+// scalacOptions += "-deprecation"
---
+++ myloop/src/main/scala/Loop.scala
@@ -0,0 +1,46 @@
+/*
+  The code here demonstrates how one can build there own control
+  structures in Scala.
+
+  The key features which makes this possible are:
+    1. Everything is an expression
+    2. Pass by name
+
+
+ */
+
+import scala.annotation.tailrec
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    loopN(10) {
+      println("Hello world!")
+    }
+
+    var i = 3
+    whilst(i > 0){
+      println("This is my while loop, i = %d".format(i))
+      i = i - 1
+    }
+  }
+
+  @tailrec
+  def loopN(n: Int)(codeBlock: => Unit): Unit = {
+    if (n > 0) {
+      codeBlock
+      loopN(n-1)(codeBlock)
+    }
+  }
+
+  /* Borrowed from
+     https://alvinalexander.com/scala/how-to-create-control-structures-dsls-in-scala-cookbook
+  */
+  @tailrec
+  def whilst(testCondition: => Boolean)(codeBlock: => Unit) {
+    if (testCondition) {
+      codeBlock
+      whilst(testCondition)(codeBlock)
+    }
+  }
+}
